9451eb27952abce33a7597aa69d67610c3901c1e
Alexandru Rinciog <uwdfx@student.kit.edu>
2017-06-06 11:03:25 +0200

    added separate methods for plot management

--- src/model/logistic_regression.py
+++ src/model/logistic_regression.py
@@ -63,7 +63,7 @@ class LogisticRegression(Classifier):
         iteration = 0
         accuracy = []
         error_progresion = []
-        pl.ion()
+        self._initialize_plot()
         legend_exists = False
 
         #Train for some epochs if the error is not 0
@@ -87,30 +87,9 @@ class LogisticRegression(Classifier):
                 learned = True
             accuracy.append(accuracy_score(self.trainingSet.label, hypothesis))
             error_progresion.append(totalError)
-            x = range(iteration)
-            pl.xlabel(u"Epochs")
-            pl.xlim(0, self.epochs)
-            pl.ylim(0, 1.0)
-            pl.plot(x, accuracy, 'k', label='accuracy')
-            pl.plot(x, error_progresion, 'r', label='mean sqared \nerror')
-            if not legend_exists:
-                # Now add the legend with some customizations.
-                legend = pl.legend(bbox_to_anchor=(1.5, 1.0))
-
-                # The frame is matplotlib.patches.Rectangle instance surrounding the legend.
-                frame = legend.get_frame()
-                frame.set_facecolor('0.90')
-
-                # Set the fontsize
-                for label in legend.get_texts():
-                    label.set_fontsize('large')
-
-                for label in legend.get_lines():
-                    label.set_linewidth(1.5)  # the legend line width
-                legend_exists = True
-                pl.subplots_adjust(left=0.1, right=0.7, top=0.9, bottom=0.1)
-            pl.show()
-            pl.pause(0.01)
+            legend_exists= self._update_plot(iteration,
+                              accuracy, error_progresion,
+                              legend_exists)
 
 
     def classify(self, testInstance):
@@ -155,3 +134,32 @@ class LogisticRegression(Classifier):
         # Look at how we change the activation function here!!!!
         # Not Activation.sign as in the perceptron, but sigmoid
         return Activation.sigmoid(np.dot(np.array(input), self.weight))
+
+    def _initialize_plot(self):
+        pl.ion()
+
+    def _update_plot(self, iteration, accuracy, error_progresion, legend_exists):
+        x = range(iteration)
+        pl.xlabel(u"Epochs")
+        pl.xlim(0, self.epochs)
+        pl.ylim(0, 1.0)
+        pl.plot(x, accuracy, 'g-', label='accuracy')
+        pl.plot(x, error_progresion, 'r-', label='mean sqared \nerror')
+        if not legend_exists:
+            # Now add the legend with some customizations.
+            legend = pl.legend(loc='upper right')
+
+            # The frame is matplotlib.patches.Rectangle instance surrounding the legend.
+            # frame = legend.get_frame()
+            # frame.set_facecolor('0.90')
+
+            # Set the fontsize
+            for label in legend.get_texts():
+                label.set_fontsize('large')
+
+            for label in legend.get_lines():
+                label.set_linewidth(1.5)  # the legend line width
+            pl.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+        pl.show()
+        pl.pause(0.01)
+        return True
