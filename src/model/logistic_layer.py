fixed gradient direction and debugged multilayer functionality

src/model/logistic_layer.py
@@ -18,6 +18,10 @@ class LogisticLayer:
     activation: string: activation function of every units in the layer
     isClassifierLayer: bool:  to do classification or regression
 
+    The bias dummy node is always first in the implicit list of nodes
+    represented through weights. Note that while the bias weight must be
+    updated in each layer, no error can be propagated through this node.
+
     Attributes
     ----------
     nIn : positive int:
@@ -89,8 +93,12 @@ class LogisticLayer:
         self.output = []
         self.input = np.append([1], np.array(layerInput))
         for i in range(0, self.nOut):
-            self.output.append(self.activation(np.dot(self.input,
-                                                      self.weights[i])))
+            try:
+                self.output.append(self.activation(np.dot(self.input,
+                                                          self.weights[i])))
+            except ValueError as e:
+                print("Failed to compute the activation of the following dot product: "
+                      + str(self.input) + " and " + str(self.weights[i]))
         return self.output
 
     # here's where the magic happens ^^
@@ -100,11 +108,10 @@ class LogisticLayer:
         1.) First we calculate the propagated output error:
         dE_dy_i = expected_i - net_i
                 = sum_{ j in {1 .. nIn}} dE_dx_j * w_ij}
-                = sum_{ j in {1 .. nIn}} nextDerivatives[j] * nextWeights[i][j]
-                = nextDerivatives * nextWeights[i], i in {1 .. nOut}
+                = sum_{ j in {1 .. nIn}} nextDerivatives[j] * nextWeights[j][i], i in {1 .. nOut}
         2.) from this we can calculate the new derivatives out of the
         activation function which we return to the caller together with the current
-        weightss:
+        weights:
         dE_dx_i = dE_dy_i * dy_i_dx_i
                 = dE_dy_i * y_i * (1 - y_i)
                 = dE_dy_i * self.activationPrime(self.output[i]), i in {1 .. nOut}
@@ -129,6 +136,7 @@ class LogisticLayer:
         ndarray :
             a numpy array containing the partial derivatives on this layer
         """
+        self.delta = []
         newDerivatives = []
         oldWeights = []
         for i in range(0, self.nOut):
@@ -136,7 +144,10 @@ class LogisticLayer:
             if self.isClassifierLayer:
                 dE_dy_i = nextDerivatives[i]
             else:
-                dE_dy_i = nextDerivatives * nextWeights[i]
+                dE_dy_i = 0
+                for j in range(0, len(nextDerivatives)):
+                    # when looking for contributions  from the layer above, skip the bias! ...[i + 1]
+                    dE_dy_i += nextDerivatives[j] * nextWeights[j][i + 1]
             # 2.) from this we can calculate the new derivatives
             dE_dx_i = dE_dy_i * self.activationPrime(self.output[i])
             newDerivatives.append(dE_dx_i)
@@ -153,4 +164,10 @@ class LogisticLayer:
         """
         # always update weights from outside the layer class
         for i in range(0, self.nOut):
-            self.weights[i] += self.learningRate * np.array(self.delta[i])
+            update_value = self.learningRate * self.delta[i]
+            if np.isnan(update_value).any():
+                raise ValueError("Through the roof! The update value for the "
+                                 "layer weights just exploded... " + str(self.delta) +
+                                 " does not bode well with a learning rate of " + str(self.learningRate))
+            self.weights[i] -= update_value
+

src/model/logistic_regression.py
@@ -110,18 +110,27 @@ class LogisticRegression(Classifier):
         """
         self.layers = []
         nIn = self.trainingSet.input.shape[1]
-        for i in range(0, len(network_representation)):
+        nLayers = len(network_representation);
+        for i in range(0, nLayers):
             # initialize layer weights:
             # a separate array of weights for each node in the layer.
             weights = []
             for j in range(0, network_representation[i]):
-                weight = np.random.rand(nIn + 1) - 0.7    # tweak initial weights here!
+                weight = np.random.rand(nIn + 1) - 0.5    # tweak initial weights here!
                 weights.append(weight)
-            layer = LogisticLayer(nIn=nIn,
-                                  nOut=network_representation[i],
-                                  activation=self.activation_string,
-                                  weights=np.array(weights),
-                                  learningRate=self.learningRate)
+            if i != nLayers - 1:
+                layer = LogisticLayer(nIn=nIn,
+                                      nOut=network_representation[i],
+                                      activation=self.activation_string,
+                                      weights=np.array(weights),
+                                      learningRate=self.learningRate,
+                                      isClassifierLayer=False)
+            else:   # mark the output layer as the classifier layer
+                layer = LogisticLayer(nIn=nIn,
+                                      nOut=network_representation[i],
+                                      activation=self.activation_string,
+                                      weights=np.array(weights),
+                                      learningRate=self.learningRate)
             self.layers.append(layer)
             nIn = network_representation[i]
 
@@ -148,7 +157,7 @@ class LogisticRegression(Classifier):
             # whenever we do a forward pass we also have to do a backw pass.
             # we compute the error, do the update and cross_validate after the loop
             for i in range(0, self.trainingSet.input.shape[0]):
-                self._get_gradient(d[i], self.trainingSet.input[i])
+                self._tune_net_parameters(d[i], self.trainingSet.input[i])
 
             # compute error
             output = list(map(lambda x: self.fire(x)[0], self.trainingSet.input))
@@ -162,9 +171,9 @@ class LogisticRegression(Classifier):
             accuracy.append(accuracy_score(self.validationSet.label,
                                            validation_output))
             
-            # stop conditions
+            # stop conditions (early stopping implemented by comparing old_score and new_score)
             iteration += 1
-            if totalError == 0 or iteration >= self.epochs or old_score > new_score:
+            if totalError == 0 or iteration >= self.epochs: # or old_score > new_score
                 learned = True
             old_score = new_score
 
@@ -176,7 +185,7 @@ class LogisticRegression(Classifier):
                               accuracy, error_progresion,
                               legend_exists)
 
-    def _get_gradient(self, d, input):
+    def _tune_net_parameters(self, d, input):
         y = self.fire(input)
         dE_dy = self.erf.calculateErrorPrime(d, np.array(y[0]))
         newDerivatives, oldWeights = ([dE_dy], None)

src/util/loss_functions.py
@@ -133,9 +133,17 @@ class BinaryCrossEntropyError(Error):
         """
         # error function derivative with respect to neuron output
         first_term = 0 if target == 0 else target * np.divide(1.0, output)
-        second_term = 0 if target == 1 else (1.0 - target) * \
-                                            np.divide(1.0, (1.0 - output)) * \
-                                            (-output)
+        if target == 1:
+            second_term = 0
+        else:
+            # here we sometimes get zero if we overshoot terribly (as in we expect a 0
+            # and we get a value infinitesimally close to one...)
+            # print("t: " + str(target) + " <---> o: " + str(output))
+            second_term = (1.0 - target) * np.divide(1.0, (1.0 - output)) * (-output)
+            if np.isnan(second_term):
+                error_string = 'Error is through the stratosfere and too close to one! ' \
+                               'We expected a value close to 0 and got ' + str(output)
+                raise ValueError(error_string)
         derivative = first_term + second_term
         #print((1.0 / self.set_size) * derivative)
         return - np.divide(1.0, self.set_size) * derivative